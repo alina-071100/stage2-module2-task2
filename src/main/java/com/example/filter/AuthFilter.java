f3cb505a07c9094f0b2cb6c4149809b23088456e
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-11-06 13:43:37 +0400

    task initial version 3

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -2,16 +2,19 @@ package com.example.filter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebFilter("/user/*")
 public class AuthFilter extends HttpFilter {
+    // stugvi "user" session atribut ka te che, ete che tanel login jsp-i vra
     @Override
     public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         if (request.getSession().getAttribute("login") == null || request.getSession().getAttribute("password") == null  ) {
-            response.sendRedirect("/");
+            response.sendRedirect("/login.jsp");
             return;
         }
         chain.doFilter(request, response);
--- src/main/java/com/example/servlet/LogoutServlet.java
+++ src/main/java/com/example/servlet/LogoutServlet.java
@@ -6,15 +6,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
+    // jnjel user atribut@ sessiayic
     @Override
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response) throws ServletException, IOException {
-        request.getSession().removeAttribute(request.getSession().getId());
-//        request.getSession().invalidate();
+
+        HttpSession session = request.getSession();
+
+        session.removeAttribute("user");
+        session.invalidate();
+
         request.getRequestDispatcher("/").forward(
                 request, response);
     }
